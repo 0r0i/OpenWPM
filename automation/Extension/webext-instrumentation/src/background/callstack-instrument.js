71c629ad31b5fc9ccd02e9af44938bef52dbf45d
Stefan Zabka <szabka@mozilla.com>
2020-01-10 17:07:09 +0100

    Apparently this function doesn't need to be awaited to be called

--- automation/Extension/webext-instrumentation/src/background/callstack-instrument.js
+++ automation/Extension/webext-instrumentation/src/background/callstack-instrument.js
@@ -3,15 +3,14 @@ export class CallstackInstrument {
     constructor(dataReceiver) {
         this.dataReceiver = dataReceiver;
     }
-    async run(crawlID) {
+    run(crawlID) {
         browser.stackDump.onStackAvailable.addListener((requestId, stack) => {
             const record = {
                 crawl_id: crawlID,
                 request_id: requestId,
                 call_stack: stack
-            }
-            console.log("Extension Callstack does this even run?")
-            await this.dataReceiver.saveRecord("callstacks", record)
+            };
+            this.dataReceiver.saveRecord("callstacks", record);
         });
     }
 }
